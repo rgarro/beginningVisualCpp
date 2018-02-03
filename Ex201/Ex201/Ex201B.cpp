861143a34cf82677fbc6af61531506a916cc417b
rgarro <rgarro@gmail.com>
2018-02-03 14:31:42 -0600

    Oranges are not the oinly fruit ...
    Oranges are not the oinly fruit ...

---
+++ Ex201/Ex201/Ex201B.cpp
@@ -0,0 +1,20 @@
+//Here we go
+#include "stdafx.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+	int apples;
+	int oranges;
+	int fruit;
+	apples = 5;
+	oranges = 6;
+	fruit = apples + oranges;
+
+	cout << endl;
+	cout << "Oranges are not the only fruit .. \n";
+	cout << "Fruits: " << fruit << "\n";
+	return 0;
+}
