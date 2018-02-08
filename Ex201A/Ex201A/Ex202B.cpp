18b6599a2b0cc17cd41ded88c97fca0012206205
rgarro <rgarro@gmail.com>
2018-02-08 13:25:54 -0600

    iomanip no es un maquillaje de disfraz, short i es un snort ....
    iomanip no es un maquillaje de disfraz ....

---
+++ Ex201A/Ex201A/Ex202B.cpp
@@ -0,0 +1,15 @@
+//wa wa wa ..
+
+#include "stdafx.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+	int num1{ 1234 }, num2{ 5678 };
+	cout << endl;
+	cout << num1 << num2;
+	cout << endl;
+	return 0;
+}
