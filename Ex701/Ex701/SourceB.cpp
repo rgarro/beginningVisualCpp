1f986df77d014b756a5cb26cce23206720184b17
rgarro <rgarro@gmail.com>
2018-03-16 14:59:31 -0600

    彼らはジャマイカで英語を話す
    彼らはジャマイカで英語を話す

---
+++ Ex701/Ex701/SourceB.cpp
@@ -0,0 +1,16 @@
+// Ex701.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using std::cout;
+
+
+int main()
+{
+
+	cout << "And the Winner is!! ";
+
+	return 0;
+}
