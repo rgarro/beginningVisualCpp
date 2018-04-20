613fc2f5ba1fd4ac1a2f4f97afa1d574fafff242
rgarro <rgarro@gmail.com>
2018-04-19 19:23:31 -0600

    年代が戻ってきます
    年代が戻ってきます

--- Ex813/Ex813/CBox.cpp
+++ Ex813/Ex813/CBox.cpp
@@ -2,11 +2,30 @@
 #include "CBox.h"
 
 
-CBox::CBox()
+
+CBox::CBox(double lv = 1.0, double wv = 1.0, double hv = 1.0):
+m_Length{std::max(lv,wv)},m_Width{std::min(lv,wv)},m_Height{hv}
 {
+	if (lv < 0.0 || wv < 0.0 || hv < 0) {
+		throw "negative dimensions";
+	}
+	if (m_Height > m_Length) {
+		std::swap(m_Height, m_Length);
+
+	}
+	else if (m_Height > m_Width) {
+		std::swap(m_Height, m_Width);
+	}
 }
 
 
 CBox::~CBox()
 {
+}
+
+
+// less than operator for box objects
+bool CBox::operator<(CBox& aBox) const
+{
+	return volume() < aBox.volume();
 }
--- Ex813/Ex813/CBox.h
+++ Ex813/Ex813/CBox.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <algorithm>
+
+using std::rel_ops::operator<=;
+using std::rel_ops::operator>;
+using std::rel_ops::operator>=;
+using std::rel_ops::operator!=;
+
 class CBox
 {
 public:
 	CBox();
+	explicit CBox(double lv = 1.0,double wv = 1.0,double hv = 1.0);
 	~CBox();
+private:
+	double m_Length;
+	double m_Width;
+	double m_Height;
+public:
+	// less than operator for box objects
+	bool operator<(CBox& aBox);
 };
 
